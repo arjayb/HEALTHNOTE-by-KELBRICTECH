3d6d2f653d2f2683c08ce530fb8b5c099c080913
KELBRIC Technologies <arjayb.fb@gmail.com>
2026-08-25 21:01:06 +0800

    fix: load acceptance UI fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/base.css";
 import "./styles/screens.css";
+import "./styles/fixes.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
